7f9fe99f0d810e6b5dcdc8b08c065ceed88b9a3f
Gabe Tanenhaus <gabe@haus.video>
2024-11-05 10:52:16 -0500

    sync

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("login-button").addEventListener("click", function() {
+    // Clear the input fields on click
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+});
